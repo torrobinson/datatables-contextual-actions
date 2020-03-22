577ce6d2be2187dc3bd382e95d7b33f2a13290b8
Tor <torrobinson@gmail.com>
2020-03-21 21:57:39 -0600

    Added url to license header

--- src/dataTables.contextualActions.js
+++ src/dataTables.contextualActions.js
@@ -2,6 +2,8 @@
 
 Copyright (c) 2020 Tor Robinson
 
+https://github.com/torrobinson/datatables-contextual-actions
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
